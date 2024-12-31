36352829711bc991641a2fd27e16f3f5200af0ab
vatikalashashank <vatikalashashank@gmail.com>
2024-12-31 09:00:24 +0530

    Create tkts=[1,2,3,4,5].py

---
+++ tkts=[1,2,3,4,5].py
@@ -0,0 +1,31 @@
+# tkts=[1,2,3,4,5]
+# print('avilable tkts',tkts)
+# user_input=int(input('enter how many tkts u wana buy'))
+# for x in range(user_input):
+#     tkt_num=int(input('enter tkt number'))
+#     tkts.remove(tkt_num)
+# print('avialable tkts',tkts)
+
+# lst=[1,2,3,4,5,1,2]
+# emp_lst=[]
+# for x in lst:
+#     if x not in emp_lst:
+#         emp_lst.append(x)
+# print(emp_lst)
+
+# lst=[1,2,3,4,5,1,2]
+# emp_lst=[]
+# c=[emp_lst.append(x)]
+        
+# print(c)
+
+
+# lst=[1,2,3,4,1,2]
+# emp_lst=[]
+# dupl_lst=[]
+# for x in lst:
+#     if x  in emp_lst:
+#         dupl_lst.append(x)
+#     else:
+#         emp_lst.append(x)
+# print(dupl_lst)
